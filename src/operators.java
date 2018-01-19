49ff48a887f17a95e245940ff4884eb55ef72356
raulm <raul.muser@gmail.com>
2018-01-19 22:55:39 +0200

    Operators class

--- src/operators.java
+++ src/operators.java
@@ -10,5 +10,30 @@ public class operators {
         double myRemainder = mySum%40;
         if(myRemainder <= 20)
             System.out.println("Total was over the limit");
+
+
+
+        // CHALLENGE
+
+        int score = 800;
+        int levelCompleted = 5;
+
+        int bonus = 100;
+        boolean gameOver = true;
+        if(gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+        score = 10_000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if(gameOver){
+
+            int finalScore = score + (levelCompleted*bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+
     }
 }
