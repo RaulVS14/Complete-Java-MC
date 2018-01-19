7ca7d318c5784c8be2b2df9a712faa4d7a09ac8a
raulm <raul.muser@gmail.com>
2018-01-19 07:14:24 +0200

    Getting to know the primitive data types - char and boolean

---
+++ src/charsAndBooleans.java
@@ -0,0 +1,15 @@
+public class charsAndBooleans {
+    public static void main(String[] args) {
+        // width 16
+        char myChar ='\u00A9';
+        System.out.println(myChar);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+
+
+        // Challenge
+        char mySecondChar = '\u00AE';
+        System.out.println(mySecondChar);
+    }
+}
