765e75ca306e373e868324547f0e662827ce2a25
raulm <raul.muser@gmail.com>
2018-01-19 07:03:17 +0200

    Getting to know the primitive data types - float and double

--- src/Main.java
+++ src/Main.java
@@ -33,5 +33,20 @@ public class Main {
         long myChallengeLong = 50000L + 30L * (myChallengeByte + myChallengeShort + myChallengeInt);
         System.out.println(myChallengeLong);
 
+
+        int myIntValue = 5 / 2;
+
+        // width 32 (4 bytes)
+        float myFloatValue = 5f / 3f;
+
+
+        double myDoubleValue = 5d / 3d;
+        System.out.println(myIntValue);
+        System.out.println(myFloatValue);
+        System.out.println(myDoubleValue);
+
+        double myPnds = 200d;
+        double myKgs = myPnds * 0.45_35_92_37d;
+        System.out.println("My kilograms " + myKgs);
     }
 }
