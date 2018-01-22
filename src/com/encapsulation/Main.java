6dbad54b2a214a4312b8b70cbf405be65c77fa94
raulm <raul.muser@gmail.com>
2018-01-22 05:24:18 +0200

    Added encapsulation package fix

--- src/com/encapsulation/Main.java
+++ src/com/encapsulation/Main.java
@@ -2,7 +2,7 @@ package com.encapsulation;
 
 public class Main {
     public static void main(String[] args) {
-        Printer myPrinter = new Printer(100.00,0,false);
+        Printer myPrinter = new Printer(100.00,false);
         myPrinter.printPages(27,"It's my life, it's do or die");
     }
 }
