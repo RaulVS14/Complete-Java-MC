2248e3d96e8d990255228459860012b17e695795
raulm <raul.muser@gmail.com>
2018-02-09 05:32:15 +0200

    Added arrayListChallenge package

---
+++ src/com/arrayListChallenge/Contacts.java
@@ -0,0 +1,4 @@
+package com.arrayListChallenge;
+
+public class Contacts {
+}
---
+++ src/com/arrayListChallenge/MobilePhone.java
@@ -0,0 +1,27 @@
+package com.arrayListChallenge;
+
+import java.util.ArrayList;
+
+public class MobilePhone {
+    ArrayList<String> Contacts = new ArrayList<String>();
+
+    public void addContact() {
+
+    }
+
+    public void modifyContact() {
+
+    }
+
+    public void removeContact() {
+
+    }
+
+    public void retrieveContact() {
+
+    }
+
+    private int findCOntact(String clientName) {
+        return Contacts.indexOf(clientName);
+    }
+}
